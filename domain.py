a8a4d47e75952ef369fb7e45ad4092b17c4c6d47
teaganryley <teaganschultz@gmail.com>
2018-08-16 14:36:38 -0400

    general functionality complete; polishing incoming

--- domain.py
+++ domain.py
@@ -5,22 +5,27 @@ class Domain:
     def __init__(self, id=None, name=None):
         self.id = id
         self.name = name
-        # TODO: do some input validation
         self.mac_address = self.generate_mac_address()
 
     def generate_mac_address(self, num_of_ids=10):
-        for i in range(number_of_ids):
+        mac_list = []
+        for i in range(num_of_ids):
             id_hex = self.int_to_hex(self.id)
-            uuid_hex = self.generate_uuid_32(self.id, self.name, 10)
-            #append
-        return self.format_mac_address(id_hex, uuid_hex)
+            uuid_hex = self.generate_uuid4()
+            mac_list.append(self.format_mac_address(id_hex, uuid_hex))
+        return mac_list
 
-    def int_to_hex(self, my_id):
-        # TODO: is it better to remove id argument, and refer to self.id instead?
+    def int_to_hex(self, my_id=0):
         # formats id as hex with leading zeroes (if applicable)
         return '{0:04X}'.format(my_id)
 
+    def generate_uuid4(self):
+        # generates uuid version 4 and slices the first 32 bits
+        return str(uuid.uuid4())[:8]
+
     def format_mac_address(self, id_hex, uuid_hex):
-        # might be unnecessary
-        pass
+        # concatenates domain id and uuid4, then formats with colon
+        li = id_hex + uuid_hex
+        mac = ':'.join((li[0:2], li[2:4], li[4:6], li[6:8], li[8:10], li[10:12]))
+        return mac
 
--- test_domain.py
+++ test_domain.py
@@ -1,4 +1,4 @@
-import pytest, uuid
+import pytest
 from domain import Domain
 
 
@@ -15,19 +15,21 @@ def test_int_to_hex(sample_domain):
     assert sample_domain.int_to_hex(48059) == 'BBBB'
     assert sample_domain.int_to_hex(52428) == 'CCCC'
 
+
 def test_int_to_hex_leading_zeroes(sample_domain):
     """Tests if method returns leading zeroes for 1, 2, and 3 digit decimal IDs"""
     assert sample_domain.int_to_hex(0) == '0000'
+    assert sample_domain.int_to_hex() == '0000'
     assert sample_domain.int_to_hex(1) == '0001'
     assert sample_domain.int_to_hex(16) == '0010'
     assert sample_domain.int_to_hex(256) == '0100'
 
 
-def test_format_mac_address():
-    #verify length
-    #verify formatting
-    pass
+def test_generate_uuid4(sample_domain):
+    """Test length and validity of generated uuid version 4."""
+    assert len(sample_domain.generate_uuid4()) == 8
+    assert int(sample_domain.generate_uuid4(), 16)
 
 
-#TODO:  look up text formatting in AtBS
-#TODO: consider adding collision resolution
+def test_format_mac_address(sample_domain):
+    assert sample_domain.format_mac_address('AAAA', 'BBCCDDEE') == 'AA:AA:BB:CC:DD:EE'
